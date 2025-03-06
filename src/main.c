180b1cecb8758177dc08a493060b9b90cdac57d2
Santiago Largo Botero <slargo-b@c1r3s1.42madrid.com>
2025-03-06 04:11:20 +0100

    parseo1

--- src/main.c
+++ src/main.c
@@ -6,18 +6,27 @@
 /*   By: slargo-b <slargo-b@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 03:38:01 by slargo-b          #+#    #+#             */
-/*   Updated: 2025/03/06 03:40:48 by slargo-b         ###   ########.fr       */
+/*   Updated: 2025/03/06 03:55:16 by slargo-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-static void free_map(t_map *map)
+static void	free_map(t_map *map)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	if (!map)
 		return ;
-	if (map->)
+	if (map->point)
+	{
+		while (i < map->row)
+		{
+			free(map->point[i]);
+			i++;
+		}
+		free(map->point);
+	}
+	free(map);
 }
--- src/parse.c
+++ src/parse.c
@@ -6,13 +6,36 @@
 /*   By: slargo-b <slargo-b@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 21:30:29 by slargo-b          #+#    #+#             */
-/*   Updated: 2025/03/05 22:00:47 by slargo-b         ###   ########.fr       */
+/*   Updated: 2025/03/06 04:07:44 by slargo-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-t_map	*my_parse(char *txt)
+int count_col(char *line)
+{
+	int i;
+	int col;
+	int len;
+
+	i = 0;
+	col = 0;
+	len = 0;
+	while (line[i])
+	{
+		if (line[i] != ' ' && !len)
+		{
+			len = 1;
+			col++;
+		}
+		else if (line[i] == ' ')
+			len = 0;
+		i++;
+	}
+	return (col);
+}
+
+t_map	*parse(char *txt, int row, int col)
 {
 	char	*line;
 	int		fd;
@@ -24,5 +47,25 @@ t_map	*my_parse(char *txt)
 		return (free (map), NULL);
 	map->row = count_lines(fd);
 	close(fd);
+
+	fd = open(txt, O_RDONLY);
+	line = get_next_line(fd);
+	if (!line)
+	{
+		return (free_map(map), NULL);
+		close(fd);
+	}
+	row = count_row(line);
+	free (line);
+	close(fd);
+
+	map = malloc(sizeof(t_map));
+	if (!map)
+		return (free (map), NULL);
+	init_map(map, row, col);
+	if (!map->grid)
+		return (free_map(map), NULL);
+	fill_points(txt, map);
+	return (map);
 }
 
