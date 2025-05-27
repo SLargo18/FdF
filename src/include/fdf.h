db60a487ce37f0420727919ac385793c5c7227b6
Santiago Largo Botero <slargo-b@c2r18s6.42madrid.com>
2025-05-27 12:01:03 +0200

    rdy

--- src/include/fdf.h
+++ src/include/fdf.h
@@ -6,7 +6,7 @@
 /*   By: slargo-b <slargo-b@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 04:53:19 by slargo-b          #+#    #+#             */
-/*   Updated: 2025/05/13 12:34:21 by slargo-b         ###   ########.fr       */
+/*   Updated: 2025/05/27 12:00:36 by slargo-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <math.h>
-# include "../../minilibx-linux/mlx.h"
+//# include "../../minilibx-linux/mlx.h"
 # include <stdio.h>
 
 # define WW 1800
